62e0d057563d6b5a5768471f773d4b3f495330c9
thrwat zidan <you@example.com>
2018-05-29 23:42:34 -0700

    tiny change

--- app/src/main/java/com/example/thrwat_zidan/myapplication/ItemListActivity.java
+++ app/src/main/java/com/example/thrwat_zidan/myapplication/ItemListActivity.java
@@ -47,7 +47,7 @@ public class ItemListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "PUT YOUR ACTION", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
